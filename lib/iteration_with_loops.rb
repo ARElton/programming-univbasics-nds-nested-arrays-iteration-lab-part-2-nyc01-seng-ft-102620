31519bfc3ec9b99f5fd1c03a949733ab7d2893eb
Amelia Elton <ameliareneeelton@gmail.com>
2020-10-07 14:49:23 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,7 +3,7 @@ def find_min_in_nested_arrays(src)
  row_index = 0 
  while row_index < src.count do
    element_index = 0 
-   lowest_temp == nil 
+   lowest_temp = nil 
    while element_index < src[row_index].count do
      if lowest_temp == nil || scr[row_index][element_index] > lowest_temp
        lowest_temp = src[row_index][element_index]
